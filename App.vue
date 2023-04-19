35642dd4a32977155c11e11526fe1e4cfd9b614b
Laura Gabriele <lauragabriele2015@gmail.com>
2023-04-19 09:23:54 -0300

    criada tabela de transações

---
+++ App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<template>
+  <TheHeader />
+</template>
+
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    table:{
+      description:"",
+      value:"",
+      date:""
+    }
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  getters: {
+  },
+  modules: {
+  }
+})
